cdc066fe3ece5670abaf80e3f760a78d5343c2a2
hjpotter92 <git@hjpotter92.email>
2023-12-19 06:41:26 +0530

    Add JWT support for fetching private streams

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@
 import { FormEvent, useEffect, useState, useRef } from "react";
 import TableComponent from "../components/TableComponent";
 
+interface FormFields {
+  ecosystem: string;
+  playbackId: string;
+  prefix: string;
+  jwt: string;
+}
+
 const regionalNodes: {
   [ecosystem: string]: IRegionNode[];
 } = {
@@ -40,11 +47,12 @@ function getUrl(
   return `https://${region}-${ecosystem}-catalyst-${index}.${host}/hls/${prefix}+${playbackId}/index.m3u8`;
 }
 
-async function getTimings(url: string): Promise<ITimingResult> {
+async function getTimings(url: string, jwt: string): Promise<ITimingResult> {
+  const jwtHeader = { "Livepeer-Jwt": jwt };
   try {
     const start = Date.now();
     console.log(`fetching ${url} at ${start}`);
-    const response = await fetch(url);
+    const response = await fetch(url, { headers: jwtHeader });
     const end = Date.now();
     const body = await response.text();
     return {
@@ -70,13 +78,13 @@ async function getTimings(url: string): Promise<ITimingResult> {
   }
 }
 
-function loopRegions(ecosystem: string, playbackId: string, prefix: string) {
+function loopRegions({ ecosystem, playbackId, prefix, jwt }: FormFields) {
   const regions = regionalNodes[ecosystem as string];
   let promises = [];
   for (const { region, count } of regions) {
     for (let index = 0; index < count; index++) {
       let url = getUrl(region, ecosystem, index, playbackId, prefix);
-      promises.push(getTimings(url));
+      promises.push(getTimings(url, jwt));
     }
   }
   return Promise.all(promises);
@@ -86,54 +94,73 @@ export default function Home() {
   const [data, setData] = useState<ITimingResult[]>([]);
   const idRef = useRef<HTMLInputElement>(null),
     prefixRef = useRef<HTMLSelectElement>(null),
-    ecosystemRef = useRef<HTMLSelectElement>(null);
+    ecosystemRef = useRef<HTMLSelectElement>(null),
+    jwtRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    const id = idRef.current?.value,
-      prefix = prefixRef.current?.value,
-      ecosystem = ecosystemRef.current?.value;
+    const id = idRef.current?.value || "",
+      prefix = prefixRef.current?.value || "",
+      ecosystem = ecosystemRef.current?.value || "",
+      jwt = jwtRef.current?.value || "";
     console.log(id, prefix, ecosystem);
-    if ([ecosystem, prefix, id].indexOf(undefined) !== -1) {
+
+    if ([ecosystem, prefix, id].indexOf("") !== -1) {
       setData([]);
       return;
     }
-    let results = await loopRegions(
-      ecosystem as string,
-      id as string,
-      prefix as string
-    );
+
+    let results = await loopRegions({
+      playbackId: id,
+      ecosystem,
+      prefix,
+      jwt,
+    });
     setData(results);
     console.log("got results");
   };
 
   useEffect(() => {
     console.log(`${new Date()} called effect`);
-    console.dir(data);
+    // console.dir(data);
   }, [data]);
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="id">Video playback ID</label>
-        <input type="text" id="id" name="id" ref={idRef} />
+        <fieldset>
+          <legend>required fields</legend>
+          <label htmlFor="id">Video playback ID</label>
+          <input type="text" id="id" name="id" ref={idRef} />
 
-        <label htmlFor="prefix">Prefix</label>
-        <select id="prefix" name="prefix" defaultValue="video" ref={prefixRef}>
-          <option value="video">video</option>
-          <option value="videorec">videorec</option>
-        </select>
+          <label htmlFor="prefix">Prefix</label>
+          <select
+            id="prefix"
+            name="prefix"
+            defaultValue="video"
+            ref={prefixRef}
+          >
+            <option value="video">video</option>
+            <option value="videorec">videorec</option>
+          </select>
 
-        <label htmlFor="ecosystem">Environment/Ecosystem</label>
-        <select
-          id="ecosystem"
-          name="ecosystem"
-          defaultValue="prod"
-          ref={ecosystemRef}
-        >
-          <option value="prod">prod</option>
-          <option value="staging">staging</option>
-        </select>
+          <label htmlFor="ecosystem">Environment/Ecosystem</label>
+          <select
+            id="ecosystem"
+            name="ecosystem"
+            defaultValue="prod"
+            ref={ecosystemRef}
+          >
+            <option value="prod">prod</option>
+            <option value="staging">staging</option>
+          </select>
+        </fieldset>
+
+        <fieldset>
+          <legend>optional fields</legend>
+          <label htmlFor="jwt">Admin JWT (for private streams)</label>
+          <input id="jwt" name="jwt" defaultValue="" ref={jwtRef} />
+        </fieldset>
 
         <button type="submit">Check</button>
       </form>
