b3eb30da531eaf3ac16f71177506b1343c39c2a0
hjpotter92 <git@hjpotter92.email>
2024-04-06 11:16:21 +0530

    Fix tailwind issues (hopefully)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -20,4 +23,4 @@ module.exports = {
   },
 
   plugins: [require("@tailwindcss/forms")],
-};
+}
